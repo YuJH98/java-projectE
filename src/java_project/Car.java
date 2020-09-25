af923c630b2b0b4ec9adde21de7dcb1c5cf1a204
YuJH98 <dhddl93@naver.com>
2020-09-25 18:05:51 +0900

    create car class , student class

---
+++ src/java_project/Car.java
@@ -0,0 +1,19 @@
+package java_project;
+
+public class Car {
+	Stirng name;
+	String price;
+	public Stirng getName() {
+		return name;
+	}
+	public void setName(Stirng name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+}
---
+++ src/java_project/Student.java
@@ -0,0 +1,27 @@
+package java_project;
+
+public class Student {
+	Stirng name;
+	String dept;
+	int grade;
+	public Stirng getName() {
+		return name;
+	}
+	public void setName(Stirng name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	
+}
